dc1eda02526afae388a80a7d42300c777e37bc71
Marlen Zhumakov <marlen.zhumakov@agile-track.com>
2023-10-10 14:54:21 +0600

    Final Solution

--- HomeWork-9.5/HomeWork-9.5/MainWindow.xaml.cs
+++ HomeWork-9.5/HomeWork-9.5/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace HomeWork_9._5
 {
@@ -23,6 +12,91 @@ namespace HomeWork_9._5
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SplitButton_Click(object sender, RoutedEventArgs e)
+        {
+            CheckIfTextIsNotEmpty(TextToSplit.Text);
+            ListView.ItemsSource = WordsPrinter(TextToSplit.Text);
+        }
+
+        private void InversionButton_Click(object sender, RoutedEventArgs e)
+        {
+            CheckIfTextIsNotEmpty(TextToInvert.Text);
+            InversionLabel.Content = InvertWords(TextToInvert.Text);
+        }
+
+        /// <summary>
+        /// Метод проверяет содержимое TextBox, если он пуст,
+        /// то выдает соотвествующее предупреждение
+        /// </summary>
+        /// <param name="stringToSplit"></param>
+        /// <returns></returns>
+        private void CheckIfTextIsNotEmpty(string text)
+        {
+            if(String.IsNullOrEmpty(text))
+            {
+                MessageBox.Show(
+                    "Сначала введите текст", 
+                    this.Title,
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning
+                    );
+            }
+        }
+
+        /// <summary>
+        /// Метод разделяет строку на слова, заполняет ими массив и возвращает его
+        /// </summary>
+        /// <param name="stringToSplit"></param>
+        /// <returns></returns>
+        private string[] StringIntoArrayOfWords(string stringToSplit)
+        {
+            char emptySpace = ' ';
+            string[] result = stringToSplit.Split(emptySpace, (char)StringSplitOptions.RemoveEmptyEntries);
+            return result;
+        }
+
+        /// <summary>
+        /// Метод принемает строку, передает ее в  StringIntoArrayOfWords(), 
+        /// полученный массив слов выводит на экран
+        /// </summary>
+        /// <param name="stringToSplitAndPrint"></param>
+        private ObservableCollection<string> WordsPrinter(string stringToSplitAndPrint)
+        {
+            string[] words = StringIntoArrayOfWords(stringToSplitAndPrint);
+            ObservableCollection<string> listOfWords = new ObservableCollection<string>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                listOfWords.Add(words[i]);
+            }
+
+            return listOfWords;
+        }
+
+        /// <summary>
+        /// Метод принимает строку, передает ее в StringIntoArrayOfWords,
+        /// распечатывает полученный массив слов задом наперед
+        /// и переносит на новую строку каждые 4 слова, что бы поместить
+        /// весь текст в Label
+        /// </summary>
+        /// <param name="textToInvert"></param>
+        private string InvertWords(string textToInvert)
+        {
+            string[] words = StringIntoArrayOfWords(textToInvert);
+            string inversionResult = "";
+
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                if(i % 4 == 0 && i != words.Length - 1)
+                {
+                    words[i] += "\n";
+                }
+                inversionResult += words[i] + " ";
+            }
+
+            return inversionResult;
         }
     }
 }
